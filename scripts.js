3178df3058c1009c9c5cf149af73f9d4916327ba
Chrissy Schepker <chrissyschepker@yahoo.com>
2021-03-27 17:17:52 -0500

    finished, i think...

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ function init(){
     const rightButton = document.getElementById("Right");
     const rocket = document.getElementById("rocket");
     let altitude = parseInt(document.getElementById("spaceShuttleHeight").innerHTML);
-}
+
 
 function clickTakeOffButton(){
 let response = window.confirm("Confirm that the shuttle is ready for takeoff.");
@@ -20,7 +20,7 @@ if (response) {
     document.getElementById("spaceShuttleHeight").innerHTML =+ 10000;
 }
 }
-takeOffButton.addEventListener("click", clickTakeOffButton);
+
 
 function clickLandingButton(){
     let response = window.alert("The shuttle is landing. Landing gear engaged.");
@@ -28,7 +28,7 @@ function clickLandingButton(){
     document.getElementById("shuttleBackground").style = "background-color: green";
     document.getElementById("spaceShuttleHeight").innerHTML = "0";
 }
-landingButton.addEventListener("click", clickLandingButton);
+
 
 function clickMissionAbortButton(){
 let response = window.confirm("Confirm that you want to abort the mission.");
@@ -36,12 +36,58 @@ if (response){
     document.getElementById("flightStatus").innerHTML = "Mission aborted.";
     document.getElementById("shuttleBackground").style = "background-color: green";
     document.getElementById("spaceShuttleHeight").innerHTML = 0;
-    //rocket.style.top = "250 px";
+    rocket.style.top = "250 px";
+    rocket.style.left = "0px";
 }
 }
+
+function clickLeftButton(){
+    let left = parseInt(rocket.style.left);
+    if (rocket.style.left !== "-230px") {
+        rocket.style.left = (left-10)+ "px";
+        console.log(rocket.style.left);
+    }
+}
+
+function clickRightButton(){
+    let right = parseInt(rocket.style.right);
+    if (rocket.style.right !== "230px") {
+        rocket.style.left = (left +10) + "px";
+        console.log(rocket.style.left);
+    }
+}
+
+function clickUpButton(){
+    let top = parseInt(rocket.style.top);
+    if (rocket.style.top !== "0px") {
+        rocket.style.top = (top-10) + "px";
+        altitude += 10000;
+        document.getElementById("spaceShuttleHeight").innerHTML = altitude;
+        console.log(rocket.style.top);
+    }
+}
+
+function clickDownButton(){
+    let top = parseInt(rocket.style.top);
+    if (rocket.style.top !== "250px"){
+        rocket.style.top = (top+10) + "px";
+        altitide -= 10000;
+        document.getElementById("spaceShuttleHeight").innerHTML = altitude;
+        console.log(rocket.style.top);
+    }
+}
+    
+
+
+takeOffButton.addEventListener("click", clickTakeOffButton);
+landingButton.addEventListener("click", clickLandingButton);
 missionAbortButton.addEventListener("click", clickMissionAbortButton);
+leftButton.addEventListener("click",clickLeftButton);
+rightButton.addEventListener("click", clickRightButton);
+upButton.addEventListener("click", clickUpButton);
+downButton.addEventListener("click", clickDownButton);
 
-
+}
 
 // Remember to pay attention to page loading!
 window.addEventListener("load", init);
